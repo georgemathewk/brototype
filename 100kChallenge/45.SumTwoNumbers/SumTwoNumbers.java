d87c913136bc1f99fb28c0a61ada3359024b0ad9
georgemathewk <georgemathewk@yahoo.com>
2023-10-02 12:49:25 +0530

    Sum of two numbers

---
+++ 100kChallenge/45.SumTwoNumbers/SumTwoNumbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SumTwoNumbers{
+	public static void main(String args[]){
+		
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		int b= s.nextInt();
+
+		int sum = a + b;
+		System.out.println("Sum : " + sum);
+			
+
+	}
+}
