d4784a2e76c51e4d8558a8237453e29910fbe3e1
georgemathewk <georgemathewk@yahoo.com>
2023-10-03 15:56:14 +0530

    NonDefault base class constructor call

---
+++ 100kChallenge/61.NonDefaultConstructorCallInInheritance/A.java
@@ -0,0 +1,9 @@
+class A{
+	A(){
+		System.out.println("Base Class - Default Constructor");
+	}
+
+	A(int a){
+		System.out.println("Base Class - Non default constructor");
+	}
+}
---
+++ 100kChallenge/61.NonDefaultConstructorCallInInheritance/B.java
@@ -0,0 +1,6 @@
+class B extends A{
+	B(int b){
+		super(b);
+		System.out.println("Sub class - Non default Constructor");
+	}
+}
