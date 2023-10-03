1d94bfc8bff6ec031c0f9b0caca57c942ea79526
georgemathewk <georgemathewk@yahoo.com>
2023-10-03 14:08:09 +0530

    Static Demo

---
+++ 100kChallenge/53.StaticDemo/Main.java
@@ -0,0 +1,7 @@
+public class Main{
+	public static void main(String args[]){
+		StaticDemo demo1 = new StaticDemo();
+		StaticDemo.fun1();
+		demo1.fun2();
+	}
+}
---
+++ 100kChallenge/53.StaticDemo/StaticDemo.java
@@ -0,0 +1,12 @@
+class StaticDemo{
+	static int a;
+	int b;
+	public static void fun1(){
+		System.out.println("a : " + a);
+	}
+
+	public void fun2(){
+		System.out.println("b : " + b);
+	}
+
+}
